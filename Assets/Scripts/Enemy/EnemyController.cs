a0e9d1d8c394251b074ce48eb4eea13f0e6487d3
Zariay <alexswfan@gmail.com>
2016-06-08 21:44:06 -0700

    On Trigger bounce

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -32,7 +32,7 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Bounce"))
             moveSpeed *= -1;
