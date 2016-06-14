c71e4fccbf60d53cadf9f4ce236e65b37144a498
Zariay <alexswfan@gmail.com>
2016-06-14 16:49:08 -0700

    PlayerTimer

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -7,6 +7,7 @@ public class LevelManager : MonoBehaviour
     PlayerController player;
     PlayerHealth pHealth;
     UnityStandardAssets._2D.Camera2DFollow playerCam;
+    Objectives objectives;
 
     public Slider playerHealth;
     void Start()
@@ -14,11 +15,18 @@ public class LevelManager : MonoBehaviour
         player = FindObjectOfType<PlayerController>();
         pHealth = player.GetComponent<PlayerHealth>();
         playerCam = FindObjectOfType<UnityStandardAssets._2D.Camera2DFollow>();
+        objectives = FindObjectOfType<Objectives>();
     }
 
     void Update()
     {
         if (player.lives < 0)
+        {
+            player.enabled = false;
+            playerCam.isFollowing = false;
+        }
+
+        if(objectives.currentTime <= 0.0f)
         {
             player.enabled = false;
             playerCam.isFollowing = false;
