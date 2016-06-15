7b75ea4735163a6caa3af446c8a369f6f45e0641
wompireteeth <376481@AIV.EDMC.STU>
2016-06-14 19:19:08 -0700

    Ross2

--- Assets/Scripts/Player/BulletDamage.cs
+++ Assets/Scripts/Player/BulletDamage.cs
@@ -39,10 +39,6 @@ public class BulletDamage : MonoBehaviour
             other.GetComponent<EnemyHealth>().SendMessage("TakeDamage", damage);
         }*/
            
-       if(other.CompareTag("BreakableBlock"))
-        {
-            Destroy(other.gameObject);
-            Destroy(this.gameObject);
-        }
+       
     }
 }
