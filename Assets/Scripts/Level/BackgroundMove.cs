27db280b8c750eceef1787e8aa32f75a7de32fd9
Mitchellablitt <mitchellablitt@hotmail.com>
2016-06-14 20:10:08 -0700

    mitchellAssets_ver2

---
+++ Assets/Scripts/Level/BackgroundMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMove : MonoBehaviour {
+
+	 public float movementSpeed = 10;
+
+     void Update()
+     {
+         transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+     }
+}
